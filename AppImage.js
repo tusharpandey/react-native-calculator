418bee3fe11dcb9323e9cade079f70287b134f55
tusharpandey <tushar.pandey@bold.com>
2019-07-22 17:14:15 +0530

    ui created for operation screen

--- AppImage.js
+++ AppImage.js
@@ -1,5 +1,10 @@
 const AppImage = {
 	splashLogo: require('./assets/calculator.png'),
+
+	addition: require('./assets/add.png'),
+	substraction: require('./assets/substract.png'),
+	multiplcation: require('./assets/multiply.png'),
+	divide: require('./assets/divide.png'),
 };
 
 export default AppImage;
--- Operation.js
+++ Operation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Image } from 'react-native';
+import { StyleSheet, View, TextInput, Image } from 'react-native';
 import AppImage from './AppImage'
 
 export default class Operation extends React.Component {
@@ -10,11 +10,46 @@ export default class Operation extends React.Component {
 
     render() {
         return (
-            <View>
-                <View>
-                    <Image source={AppImage.splashLogo} />
+            <View style={styles.container}>
+                
+                <View style={styles.userInput}>
+                    <TextInput keyboardType = 'numeric' placeholder = "input" />
                 </View>
+                
+                <Image style={styles.operationContainer} source={AppImage.addition} />
+
+                <View style={styles.userInput}>
+                    <TextInput keyboardType = 'numeric' placeholder = "input" />
+                </View>
+
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 18,
+        marginLeft: 18,
+        marginRight: 18,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+
+    userInput: {
+        paddingLeft:8,
+        flex: 3,
+        borderWidth: 0.5,
+        borderColor: 'gray',
+        height: 25,
+    },
+
+    operationContainer: {
+        marginLeft: 15,
+        marginRight: 15,
+        flex: 1,
+        width: 25,
+        height: 25,
+    }
+});
